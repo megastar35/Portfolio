30153d498a23e410eac3968431505bd9cfbf6747
Dũng Tri <lieudungtri@gmail.com>
2022-05-31 22:16:36 +0700

    Update v1

---
+++ js/materialize_initial.js
@@ -0,0 +1,18 @@
+// //MATERIALIZE CSS//
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('.scrollspy');
+//     var instances = M.ScrollSpy.init(elems, { activeClass: "active" });
+// });
+
+// add ACTIVE CLASS in skillbar section
+
+// TOOLTIPS
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems);
+});
+// COLLAPSIBLE
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+  });
